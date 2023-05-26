3ebaded4c027c43264835383dd8e1b7b6c3fcceb
agefs027560jc <agefs027560jc@no.email>
2023-05-26 20:34:33 +0700

    fix deprecated deps

--- dep.sh
+++ dep.sh
@@ -1,3 +1,5 @@
+set -x -e
+
 sudo apt-get --assume-yes update
 
 sudo apt-get --assume-yes install -y \
@@ -24,5 +26,6 @@ sudo apt-get --assume-yes install -y \
 sudo wget https://github.com/mikefarah/yq/releases/download/v4.24.2/yq_linux_amd64 \
     -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 
+pip3 install --upgrade dev pip wheel setuptools
 pip3 install -r requirements.txt
 pip3 install Pillow matplotlib pyyaml
